e94c9ed8337b402c93ed9f89fcf82272d46ec829
skmothi19 <skmothi19@gmail.com>
2025-06-12 12:39:51 +0400

    Update getAllTokens.ts

--- src/pages/api/getAllTokens.ts
+++ src/pages/api/getAllTokens.ts
@@ -8,11 +8,13 @@ const pool = new Pool({
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
+    console.log("Connection string: ",env.NEON_DB_API_KEY);
     // Get all data from the tokens table
     const { rows } = await pool.query('SELECT * FROM token_details');
+    console.log("Query Results: ", rows);
     res.status(200).json({ result: rows });
   } catch (error) {
-    console.error(error);
+    console.error("Error in API: ",error);
     res.status(500).json({ error: 'Failed to fetch all tokens' });
   }
 } 
