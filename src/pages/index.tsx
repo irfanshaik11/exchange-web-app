9f4e3abc30ccb91b96e73d5db8527143b896b130
Mehul Sharma <66416012+mehuylsharma@users.noreply.github.com>
2025-07-25 11:58:15 +0530

    i zo da change wit za phone

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -168,10 +168,7 @@ export default function Home() {
       const data = await tradeBuy({
         tokenAddress: token.mint,
         amount: quickBuyAmount,
-        mevProtection: presets[activePreset].quickBuySettings.mevMode === "off" ? 0 : 1,
-        solPrice: token.sol_price,
-        marketCap: token.total_fully_diluted_valuation,
-        tokenPrice: token.usd_price,
+        mevProtection: presets[activePreset].quickBuySettings.mevMode === "off" ? 0 : 1
       }, user.bearerToken);
       toast.success(
         `Quick Buy successful! Bought ${data.amount} ${token.symbol}`,
@@ -221,7 +218,7 @@ export default function Home() {
   useEffect(() => {
     if (presets && presets[activePreset]) {
       // Optionally, update quickBuySettings globally if needed
-      // setQuickBuySettings(presets[activePreset].quickBuySettings);
+      setQuickBuySettings(presets[activePreset].quickBuySettings);
     }
   }, [activePreset, presets]);
 
