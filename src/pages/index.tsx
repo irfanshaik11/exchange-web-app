a4620e4de6646115a81e5101839cb3e4f75508ad
Mehul Sharma <66416012+mehuylsharma@users.noreply.github.com>
2025-07-25 12:01:01 +0530

    my phone zanzes not worki

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -218,7 +218,7 @@ export default function Home() {
   useEffect(() => {
     if (presets && presets[activePreset]) {
       // Optionally, update quickBuySettings globally if needed
-      setQuickBuySettings(presets[activePreset].quickBuySettings);
+      // setQuickBuySettings(presets[activePreset].quickBuySettings);
     }
   }, [activePreset, presets]);
 
