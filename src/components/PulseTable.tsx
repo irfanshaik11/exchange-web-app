26e584d286bc90c1ccdcaf3d7dd824ca59b82b09
Mehul Sharma <the8bitmehul@gmail.com>
2025-08-01 23:24:31 +0530

    pulse table mint

--- src/components/PulseTable.tsx
+++ src/components/PulseTable.tsx
@@ -128,7 +128,7 @@ export default function PulseTable({ title, tokens, isFirstOrLast, loading = fal
             <div
               key={token.mint + idx}
               className="relative cursor-pointer flex flex-row py-3 transition group items-center border-b border-neutral-800 hover:bg-neutral-800/40 w-full"
-              onClick={() => router.push(`/trade/${token.pair_address}`)}
+              onClick={() => router.push(`/trade/${token.mint}`)}
             >
               {/* Bonding popout on hover */}
               {idx === 0 ? (
