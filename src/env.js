19844ceba7be9878421cae7bf5d57fad96ab38de
Mehul Sharma <the8bitmehul@gmail.com>
2025-08-25 14:49:03 +0530

    env fix

--- src/env.js
+++ src/env.js
@@ -8,8 +8,6 @@ export const env = createEnv({
    */
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
-    MORALIS_API_KEY: z.string().optional(),
-    NEON_DB_API_KEY: z.string().optional(),
   },
 
   /**
@@ -18,11 +16,9 @@ export const env = createEnv({
    * `NEXT_PUBLIC_`.
    */
   client: {
-    NEXT_PUBLIC_API_URL:z.string().url().optional(),
     NEXT_PUBLIC_TOKEN_SERVICE_URL: z.string().url().optional(),
     NEXT_PUBLIC_BACKEND_URL: z.string(),
     NEXT_PUBLIC_IS_BACKEND_DEPLOYED: z.preprocess((val) => val === 'true' || val === true, z.boolean()),
-    NEXT_PUBLIC_WEBSOCKET_URL: z.string().optional(),
     
     // NEXT_PUBLIC_CLIENTVAR: z.string(),
   },
@@ -34,12 +30,8 @@ export const env = createEnv({
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
     NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
-    MORALIS_API_KEY: process.env.MORALIS_API_KEY,
-    NEON_DB_API_KEY: process.env.NEON_DB_API_KEY,
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     NEXT_PUBLIC_TOKEN_SERVICE_URL: process.env.NEXT_PUBLIC_TOKEN_SERVICE_URL,
     NEXT_PUBLIC_IS_BACKEND_DEPLOYED: process.env.NEXT_PUBLIC_IS_BACKEND_DEPLOYED,
-    NEXT_PUBLIC_WEBSOCKET_URL: process.env.NEXT_PUBLIC_WEBSOCKET_URL || 'wss://api.interstate.example.com/ws',
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
   },
   /**
