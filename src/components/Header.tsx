92cc5053866b6903c6e81d91e004691ed24b74c4
ojasvasri <ojasva@interstate.so>
2025-12-02 21:21:14 -0500

    dummy commit (#362)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1116,7 +1116,6 @@ export default function Header({
                   </div>
                   <FiChevronDown className="text-neutral-500 hover:text-neutral-200" size={16} />
                 </button>
-
                 {/* Combined Dropdown */}
                 {profileMenuOpen && (
                   <div
