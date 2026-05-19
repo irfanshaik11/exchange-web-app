cc712b67b63d46b553921cdd5ec749641b3c4787
harman-interstate <harman@interstate.so>
2026-05-19 06:20:39 +0530

    removed unused link

--- src/components/MorphingArenaNav.tsx
+++ src/components/MorphingArenaNav.tsx
@@ -14,7 +14,6 @@ const arenaMenuItems = [
   { name: "Referrals", href: "/referrals", icon: "users", disabled: false },
   { name: "Leaderboard", href: "/leaderboard", icon: "chart", disabled: true },
   { name: "KOL Leaderboard", href: "/kol-leaderboard", icon: "chart", disabled: false },
-  { name: "Vision", href: "/vision", icon: "chart", disabled: false },
   { name: "Jackpot", href: "/jackpot", icon: "grid", disabled: true },
 ];
 
