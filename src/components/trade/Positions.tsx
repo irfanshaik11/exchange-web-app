fd2546bc79233cb01f2c1446024b853c80d5fb97
privdev <irfan@interstate.so>
2026-05-29 17:21:11 -0700

    add back name and ticker to portfolio page

--- src/components/trade/Positions.tsx
+++ src/components/trade/Positions.tsx
@@ -1770,7 +1770,7 @@ const Positions: React.FC<PositionsProps> = ({
                         {metadata?.name || shortAddr(displayAddress)}
                       </button>
                       <div className="text-xs text-neutral-400 font-mono truncate" title={displayAddress}>
-                        {shortAddr(displayAddress)}
+                        {metadata?.symbol ? `$${metadata.symbol}` : shortAddr(displayAddress)}
                       </div>
                     </div>
                   </div>
