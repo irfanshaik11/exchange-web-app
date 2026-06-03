59e5069db8a84a4ea22294d3f8ccab7fc5f35a0f
hujoepandiselvan <hujoe@interstate.so>
2026-06-02 17:52:01 -0700

    Revert "fix: derive avg exit price from totalSoldUsd/totalSoldTokens when not pre-computed"
    This reverts commit 32995722b48a1549a829dbc3ecffdca6b2a64183.

--- src/pages/trade/[id].tsx
+++ src/pages/trade/[id].tsx
@@ -1176,18 +1176,9 @@ export default function TradePage() {
           setPositionLinesApi(null);
           return;
         }
-        // Use pre-computed avg prices when available, otherwise derive from totals
-        const avgBuy = data.avgBuyPriceUsd ?? data.avgBuyPriceUSD
-          ?? (data.totalBoughtTokens > 0 && data.totalBoughtUsd > 0
-            ? data.totalBoughtUsd / data.totalBoughtTokens
-            : null);
-        const avgSell = data.avgSellPriceUsd ?? data.avgSellPriceUSD
-          ?? (data.totalSoldTokens > 0 && data.totalSoldUsd > 0
-            ? data.totalSoldUsd / data.totalSoldTokens
-            : null);
         setPositionLinesApi({
-          avgBuyPriceUsd: avgBuy,
-          avgSellPriceUsd: avgSell,
+          avgBuyPriceUsd: data.avgBuyPriceUsd ?? data.avgBuyPriceUSD ?? null,
+          avgSellPriceUsd: data.avgSellPriceUsd ?? data.avgSellPriceUSD ?? null,
         });
       } catch {
         setPositionLinesApi(null);
