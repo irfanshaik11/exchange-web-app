8995006348a3ceba8f1b5f60567c4ec7d771d295
privdev <irfan@interstate.so>
2026-06-17 18:39:35 -0700

    add chatbot

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -157,6 +157,14 @@ export default function Document() {
             }}
           />
         )}
+        {/* Crisp Chatbot */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+window.$crisp=[];window.CRISP_WEBSITE_ID="c1ec6637-ab80-42d4-b17e-f6a8288faf14";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();
+            `,
+          }}
+        />
       </Head>
       <body>
         <Main />
